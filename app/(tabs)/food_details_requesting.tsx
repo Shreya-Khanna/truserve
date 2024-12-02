80c63b877cd05560fadc24147689e749f257d8ad
AyushmaanWorks <186050806+AyushmaanWorks@users.noreply.github.com>
2024-12-03 02:11:17 +0530

    file updates

---
+++ app/(tabs)/food_details_requesting.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Screenshot_2024_12_02_162248 = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>User Profile</Text>
+      <Text style={styles.text}>Name</Text>
+      <Text style={styles.text}>Location</Text>
+      <Text style={styles.text}>Contact Information</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  text: {
+    marginVertical: 5,
+    fontSize: 16,
+  },
+});
+
+export default Screenshot_2024_12_02_162248;
---
+++ app/(tabs)/role_selector_screen.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+interface Props {
+  navigation: any;
+}
+
+const HomeScreen: React.FC<Props> = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Login')}
+      >
+        <Text style={styles.buttonText}>Food Provider</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('FoodItems')}
+      >
+        <Text style={styles.buttonText}>Food Receiver</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#E9EED9',
+  },
+  button: {
+    backgroundColor: '#ABBA7C',
+    paddingVertical: 120,
+    paddingHorizontal: 100,
+    marginVertical: 10,
+    borderRadius: 10,
+    alignContent:"center",
+    marginBottom:60,
+    marginTop:50
+
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 25,
+    alignItems:"center",
+    fontWeight: 'bold',
+  },
+});
+
+export default HomeScreen;
