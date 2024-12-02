ffc87ada9e2f2f0d8a6c85e84b6821f02867ee86
AyushmaanWorks <186050806+AyushmaanWorks@users.noreply.github.com>
2024-12-03 01:04:42 +0530

    sign up

--- app/(tabs)/login_form_screen.tsx
+++ app/(tabs)/login_form_screen.tsx
@@ -1,15 +1,21 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 
 const LoginScreen: React.FC = () => {
   const [form, setForm] = useState({
-    firstName: '',
-    lastName: '',
-    username: '',
-    password: '',
-    confirmPassword: '',
-    contactNumber: '',
-    email: '',
+    FirstName: "",
+    LastName: "",
+    Username: "",
+    Password: "",
+    ConfirmPassword: "",
+    ContactNumber: "",
+    Email: "",
   });
 
   const handleInputChange = (key: string, value: string) => {
@@ -18,16 +24,16 @@ const LoginScreen: React.FC = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Login Details</Text>
+      <Text style={styles.header}>Sign Up</Text>
       {Object.keys(form).map((key) => (
         <TextInput
           key={key}
-          placeholder={key.replace(/([A-Z])/g, ' $1')}
+          placeholder={key.replace(/([A-Z])/g, " $1")}
           value={form[key as keyof typeof form]}
           onChangeText={(value) => handleInputChange(key, value)}
           style={styles.input}
-          secureTextEntry={key.toLowerCase().includes('password')}
-          keyboardType={key === 'contactNumber' ? 'phone-pad' : 'default'}
+          secureTextEntry={key.toLowerCase().includes("password")}
+          keyboardType={key === "contactNumber" ? "phone-pad" : "default"}
         />
       ))}
       <TouchableOpacity style={styles.submitButton}>
@@ -40,31 +46,33 @@ const LoginScreen: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#c21e1e',
+    backgroundColor: "#E9EED9",
     padding: 20,
   },
   header: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#fff',
-    marginBottom: 20,
+    textAlign: "center",
+    marginTop: 60,
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#000",
+    marginBottom: 60,
   },
   input: {
-    backgroundColor: '#fff',
+    backgroundColor: "#000",
     borderRadius: 8,
     padding: 10,
     marginBottom: 15,
   },
   submitButton: {
-    backgroundColor: '#000',
+    backgroundColor: "#000",
     padding: 15,
     borderRadius: 10,
-    alignItems: 'center',
+    alignItems: "center",
   },
   submitButtonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
 });
 
